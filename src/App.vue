54deb4522b4dfc67c9d5ca5c5e09bf97dcfd5e97
Ryan Pays <ryan.pays@gmail.com>
2020-04-28 23:12:54 +0100

    add white color prop to icon

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,15 @@
         alt="The one and only Ryan Pays!"
       />
     </div>
-    <div class="flex justify-center text-white mt-8">
+    <div class="flex justify-center mt-8">
       <a class="mr-4" href="https://github.com/RyanPayso13">
-        <v-icon name="brands/github" scale="6" />
+        <v-icon color="white" name="brands/github" scale="6" />
       </a>
       <a href="https://www.facebook.com/ryan.pays">
-        <v-icon name="brands/facebook" scale="6" />
+        <v-icon color="white" name="brands/facebook" scale="6" />
       </a>
       <a class="ml-4" href="/doc/ryan-pays-cv.pdf" target="_blank">
-        <v-icon name="file-pdf" scale="6" />
+        <v-icon color="white" name="file-pdf" scale="6" />
       </a>
     </div>
   </section>
