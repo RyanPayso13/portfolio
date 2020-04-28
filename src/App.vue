6d9a868c9c3418a0265f4ea0d5613c0ffc896d8b
Ryan Pays <ryan.pays@gmail.com>
2020-04-28 21:44:18 +0100

    color, margins social links

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,19 @@
         alt="The one and only Ryan Pays!"
       />
     </div>
-    <div class="flex justify-center space-x-4">
-      <a href="https://github.com/RyanPayso13">
-        <v-icon class="text-white" name="brands/github" scale="6" />
+    <div class="flex justify-center text-white mt-8">
+      <a class="inline-block mr-4" href="https://github.com/RyanPayso13">
+        <v-icon name="brands/github" scale="6" />
       </a>
-      <a href="https://www.facebook.com/ryan.pays">
-        <v-icon class="text-white" name="brands/facebook" scale="6" />
+      <a class="inline-block" href="https://www.facebook.com/ryan.pays">
+        <v-icon name="brands/facebook" scale="6" />
       </a>
-      <a href="@/assets/doc/ryan-pays-cv.pdf" target="_blank">
-        <v-icon class="text-white" name="file-pdf" scale="6" />
+      <a
+        class="inline-block ml-4"
+        href="@/assets/doc/ryan-pays-cv.pdf"
+        target="_blank"
+      >
+        <v-icon name="file-pdf" scale="6" />
       </a>
     </div>
   </section>
