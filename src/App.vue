08c7acbab0e03edcdbc90e7af6031793ec1dbd48
Ryan Pays <ryan.pays@gmail.com>
2020-05-07 11:44:13 +0100

    fill valu

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,56 @@
 <template>
   <section id="app" class="h-screen bg-green-500">
-    <div class="flex justify-center">
-      <img
-        class="rounded-full border-2 border-white w-1/2 lg:w-1/4"
-        src="@/assets/img/ryan-pays-horseback.jpg"
-        alt="The one and only Ryan Pays!"
-      />
-    </div>
-    <div class="flex flex-col text-white">
-      <h1>Hi, I'm Ryan Pays!</h1>
-      <p>
-        I am a user interface engineer with over a decade of experience
-        delivering agile, lean and scalable solutions.
-      </p>
-      <p>
-        I have worked in a broad spectrum of industries ranging from sports
-        betting, consultancy, VC backed start-ups and everything inbetween.
-      </p>
-      <p>
-        If you have a project, gig or side-hustle I might be interested in
-        please feel free to
+    <div class="container mx-auto flex flex-col h-screen">
+      <div>
+        <img
+          class="rounded-full border-2 border-white w-1/2"
+          src="@/assets/img/ryan-pays-horseback.jpg"
+          alt="The one and only Ryan Pays!"
+        />
+      </div>
+      <div class="text-white">
+        <h1>Hi, I'm Ryan Pays!</h1>
+        <p>
+          I am a user interface engineer with over a decade of experience
+          delivering agile, lean and scalable solutions.
+        </p>
+        <p>
+          I have worked in a broad spectrum of industries ranging from sports
+          betting, consultancy, VC backed start-ups and everything inbetween.
+        </p>
+        <p>
+          If you have a project, gig or side-hustle I might be interested in
+          please feel free to
+          <a
+            href="mailto:ryan.pays@gmail.com?subject=Contact from ryanpays.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            >contact me!</a
+          >
+        </p>
+      </div>
+      <div>
+        <a href="https://github.com/RyanPayso13">
+          <v-icon
+            color="white"
+            class="fill-current"
+            name="brands/github"
+            scale="3"
+          />
+        </a>
+        <a href="https://www.facebook.com/ryan.pays">
+          <v-icon color="white" name="brands/facebook" scale="3" />
+        </a>
+        <a href="/doc/ryan-pays-cv.pdf" target="_blank">
+          <v-icon color="white" name="file-pdf" scale="3" />
+        </a>
         <a
           href="mailto:ryan.pays@gmail.com?subject=Contact from ryanpays.com"
           target="_blank"
-          rel="noopener noreferrer"
-          >contact me!</a
         >
-      </p>
-    </div>
-    <div class="flex justify-center mt-8">
-      <a class="mr-4" href="https://github.com/RyanPayso13">
-        <v-icon color="white" name="brands/github" scale="3" />
-      </a>
-      <a href="https://www.facebook.com/ryan.pays">
-        <v-icon color="white" name="brands/facebook" scale="3" />
-      </a>
-      <a class="ml-4" href="/doc/ryan-pays-cv.pdf" target="_blank">
-        <v-icon color="white" name="file-pdf" scale="3" />
-      </a>
+          <v-icon color="white" name="envelope" scale="3" />
+        </a>
+      </div>
     </div>
   </section>
 </template>
@@ -48,6 +61,7 @@ import Icon from "vue-awesome/components/Icon.vue";
 import "vue-awesome/icons/brands/github";
 import "vue-awesome/icons/brands/facebook";
 import "vue-awesome/icons/file-pdf";
+import "vue-awesome/icons/envelope";
 
 @Component({
   components: {
